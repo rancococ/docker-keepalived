7f3d14de3931d9bd1b3f0340bbe8472a5c53bdad
rancococ@qq.com <rancococ@qq.com>
2019-07-21 18:02:14 +0800

    update docker-startup.sh,docker-finish.sh

--- docker-finish.sh
+++ docker-finish.sh
@@ -6,7 +6,7 @@ set -e
 vips_tmp=${KEEPALIVED_VIRTUAL_IPS//,/ };
 vips_arr=($vips_tmp);
 for vip in ${vips_arr[@]}; do
-    IP=$(echo ${!vip})
+    IP=$(echo ${vip})
     IP_INFO=$(ip addr list | grep ${IP}) || continue
     IP_V6=$(echo "${IP_INFO}" | grep "inet6") || true
 
--- docker-startup.sh
+++ docker-startup.sh
@@ -102,7 +102,7 @@ fi
 vips_tmp=${KEEPALIVED_VIRTUAL_IPS//,/ };
 vips_arr=($vips_tmp);
 for vip in ${vips_arr[@]}; do
-    IP=$(echo ${!vip})
+    IP=$(echo ${vip})
     IP_INFO=$(ip addr list | grep ${IP}) || continue
     IP_V6=$(echo "${IP_INFO}" | grep "inet6") || true
 
