ddadf6659a0c15f89f3a4cc27d247854074ec14d
rancococ@qq.com <rancococ@qq.com>
2021-01-23 21:50:07 +0800

    优化启动命令行参数，支持双引号、单引号、逗号、空格等

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,8 +14,19 @@ fi
 
 # current user is root
 if [ "$(id -u)" = "0" ]; then
+    # recombine command line parameters
+    command_line_all="--dont-fork --log-console"
+	command_line_arg="${KEEPALIVED_COMMAND_LINE_ARGUMENTS}"
+    command_line_arg=$(echo ${command_line_arg} | sed "s/\"//g")
+    command_line_arg=$(echo ${command_line_arg} | sed "s/'//g")
+    command_line_arg=$(echo ${command_line_arg} | sed "s/,/ /g")
+	command_line_arg_array=(${command_line_arg})
+	for cmd in ${command_line_arg_array[@]}; do
+		command_line_all="${command_line_all} ${cmd}"
+	done
+	echo "command line parameters : ${command_line_all}"
     # start keepalived
-    exec /usr/local/sbin/keepalived -f /etc/keepalived/keepalived.conf --dont-fork --log-console ${KEEPALIVED_COMMAND_LINE_ARGUMENTS}
+    exec /usr/local/sbin/keepalived -f /etc/keepalived/keepalived.conf ${command_line_all}
 fi
 
 # exec some command
