9e95dcb1647818ce60c9f963e13a3565229ccd17
rancococ@qq.com <rancococ@qq.com>
2021-01-23 22:02:07 +0800

    替换tab为空格

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -16,15 +16,15 @@ fi
 if [ "$(id -u)" = "0" ]; then
     # recombine command line parameters
     command_line_all="--dont-fork --log-console"
-	command_line_arg="${KEEPALIVED_COMMAND_LINE_ARGUMENTS}"
+    command_line_arg="${KEEPALIVED_COMMAND_LINE_ARGUMENTS}"
     command_line_arg=$(echo ${command_line_arg} | sed "s/\"//g")
     command_line_arg=$(echo ${command_line_arg} | sed "s/'//g")
     command_line_arg=$(echo ${command_line_arg} | sed "s/,/ /g")
-	command_line_arg_array=(${command_line_arg})
-	for cmd in ${command_line_arg_array[@]}; do
-		command_line_all="${command_line_all} ${cmd}"
-	done
-	echo "command line parameters : ${command_line_all}"
+    command_line_arg_array=(${command_line_arg})
+    for cmd in ${command_line_arg_array[@]}; do
+        command_line_all="${command_line_all} ${cmd}"
+    done
+    echo "command line parameters : ${command_line_all}"
     # start keepalived
     exec /usr/local/sbin/keepalived -f /etc/keepalived/keepalived.conf ${command_line_all}
 fi
